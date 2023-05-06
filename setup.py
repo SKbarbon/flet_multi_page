f757714e3fb4343375bb8d265518b029ff4c2f08
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-05-06 20:11:28 +0300

    Add a `view` option for the page so you can choose to open a new page in a browser.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flet_multi_page',
-    version='1.2',
+    version='1.3',
     author='SKbarbon',
     description='A tool of creating new pages with flet library.',
     long_description="until now, flet v0.6 does not support multi pages.\n With this tool, you can start new pages on the same script without the need of creating new `app` class.\n visit the github repo for more: [github](https://github.com/SKbarbon/flet_multi_page)",
